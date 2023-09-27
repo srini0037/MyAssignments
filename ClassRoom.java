d09f8e1968f4f0f6fd4ed599c0c9fd4f1f49450c
srini0037 <61724340+srini0037@users.noreply.github.com>
2023-09-27 23:52:05 +0530

    Add files via upload
    week2.day2 facebook login

---
+++ ClassRoom.java
@@ -0,0 +1,24 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClassRoom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("testleaf.2023@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Tuna@321");
+		driver.findElement(By.name("login")).click();
+		driver.findElement(By.linkText("Find your account and log in.")).click();
+		driver.close();
+		
+		
+
+	}
+
+}
